880c8afbda9c19541f510196f88d2c410c5d798d
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2021-04-13 21:13:39 -0600

    Lint users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,9 @@ class UsersController < ApplicationController
     def create
         user = User.create(first_name: params[:first_name], last_name: params[:last_name], email: params[:email])
         if user.valid?
-			redirect_to "/users", alert: ["Success!"]
-		else
-			redirect_to "/users", alert: user.errors.full_messages
-		end
+    			redirect_to "/users", alert: ["Success!"]
+    		else
+    			redirect_to "/users", alert: user.errors.full_messages
+    		end
     end
 end
