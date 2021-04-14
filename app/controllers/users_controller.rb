48af61286bc6513ebecefccd3ae9eb89f22b8c38
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2021-04-13 21:11:42 -0600

    Fix flash message bug
    The view logic expects an Array :)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,10 @@ class UsersController < ApplicationController
         @users = User.all
     end
 
-    def create 
+    def create
         user = User.create(first_name: params[:first_name], last_name: params[:last_name], email: params[:email])
         if user.valid?
-			redirect_to "/users", alert: "Success!"
+			redirect_to "/users", alert: ["Success!"]
 		else
 			redirect_to "/users", alert: user.errors.full_messages
 		end
