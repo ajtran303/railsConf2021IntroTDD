9a36746d58e516658b8da40b0524410e478585db
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2021-04-14 13:22:02 -0600

    Refactor create spec to test for un/successful path

--- spec/features/create_user_spec.rb
+++ spec/features/create_user_spec.rb
@@ -3,17 +3,27 @@ feature "guest user creates an account" do
   scenario "unsuccessfully creates a new user account" do
     visit "/users"
     fill_in "first_name", with: ""
-    fill_in "last_name", with: "Pickles"
-    fill_in "email", with: "tommy@gmail.com"
+    fill_in "last_name", with: ""
+    fill_in "email", with: ""
     click_button "Create"
     expect(page).not_to have_content "Success!"
+    expect(page).to have_content "First name can't be blank"
+    expect(page).to have_content "First name is too short (minimum is 2 characters)"
+    expect(page).to have_content "Last name can't be blank"
+    expect(page).to have_content "Last name is too short (minimum is 2 characters)"
+    expect(page).to have_content "Email can't be blank"
+    expect(page).to have_content "Email is invalid"
   end
+
   scenario "successfully creates a new user account" do
+    new_user = build(:user)
     visit "/users"
-    fill_in "first_name", with: "Tommy"
-    fill_in "last_name", with: "Pickles"
-    fill_in "email", with: "tommy@gmail.com"
+    fill_in "first_name", with: new_user.first_name
+    fill_in "last_name", with: new_user.last_name
+    fill_in "email", with: new_user.email
     click_button "Create"
+    expect(current_path).to eq("/users/new")
     expect(page).to have_content "Success!"
+    expect(page).to have_content "Welcome, #{new_user.first_name}!"
   end
 end
